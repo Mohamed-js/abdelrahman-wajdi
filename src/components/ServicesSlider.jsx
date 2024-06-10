61d036ef9e3fc94e3996fc424b1466b7bd19f14c
Mohamed Z <mz7891555@gmail.com>
2024-06-10 09:32:37 +0300

    Update Services

---
+++ src/components/ServicesSlider.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import service1 from "../assets/imgs/service1.jpg";
+import service2 from "../assets/imgs/service2.jpg";
+import { useTranslation } from "react-i18next";
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
+
+// Custom Arrow components
+const CustomPrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute left-0 md:left-4 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer  bg-opacity-50 rounded-full p-2 hover:bg-opacity-75"
+      onClick={onClick}
+    >
+      <GrFormPrevious className="text-white" size={40} />
+    </div>
+  );
+};
+
+const CustomNextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute right-0 md:right-4 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer  bg-opacity-50 rounded-full p-2 hover:bg-opacity-75"
+      onClick={onClick}
+    >
+      <GrFormNext className="text-white" size={40} />
+    </div>
+  );
+};
+
+const Slide = ({ img, title, description }) => {
+  const direction = localStorage.getItem("selectedLanguage") === "ar" ? "rtl" : "ltr";
+
+  return (
+    <div className="relative h-[60vh] md:h-[90vh] w-full rounded-3xl overflow-hidden shadow-lg">
+      <div
+        className="absolute inset-0 bg-cover bg-center filter brightness-75"
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
+      <div className="absolute inset-0 flex flex-col justify-center items-start text-white px-12 md:px-32">
+        <h2
+          className={`text-xl md:text-6xl font-bold mb-4 ${
+            direction === "ltr" ? "mr-auto" : "ml-auto"
+          }`}
+        >
+          {title}
+        </h2>
+        <p
+          className={`text-lg md:text-3xl ${
+            direction === "ltr" ? "mr-auto text-left" : "ml-auto text-right"
+          } w-[80%]  lg:w-1/2`}
+        >
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const SliderComponent = () => {
+  const { t } = useTranslation();
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
+
+  const slides = [
+    {
+      img: service1,
+      title: t("services.title"),
+      description: t("services.description"),
+    },
+    {
+      img: service2,
+      title: t("services.title2"),
+      description: t("services.description2"),
+    },
+  ];
+
+  return (
+    <div className="relative w-[90%] m-auto mt-10">
+      <Slider {...sliderSettings}>
+        {slides.map((slide, index) => (
+          <div key={index}>
+            <Slide {...slide} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default SliderComponent;
--- src/screens/Services.jsx
+++ src/screens/Services.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Transition } from "react-transition-group";
 import service1 from "../assets/imgs/service1.jpg";
 import service2 from "../assets/imgs/service2.jpg";
 import service3 from "../assets/imgs/service3.jpg";
@@ -13,193 +14,103 @@ import service11 from "../assets/imgs/service11.jpg";
 import service12 from "../assets/imgs/service12.jpg";
 
 import bgImg from "../assets/imgs/bg-img.jpg";
-import {
-  FaCoffee,
-  FaBriefcase,
-  FaChartLine,
-  FaLightbulb,
-  FaStar,
-  FaRegClock,
-  FaTasks,
-} from "react-icons/fa";
-
-import { FaWhatsapp } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { AiOutlineTikTok } from "react-icons/ai";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useTranslation } from "react-i18next";
 import Footer from "../components/Footer";
+import SliderComponent from "../components/ServicesSlider";
 
 export default function Services() {
   const { t } = useTranslation();
+  // "description": "اختيارك الاوحد للاحترافية أحدث الحلول لتقديم الخدمات التسويقية من جميع الجوانب بطريقة احترفية تعكس صورتك لزيادة فرص نجاحك ونسبة مبيعاتك",
 
-  const direction =
-    localStorage.getItem("selectedLanguage") === "ar" ? "rtl" : "ltr";
+
+  const direction = localStorage.getItem("selectedLanguage") === "ar" ? "rtl" : "ltr";
 
   useEffect(() => {
     AOS.init({ duration: 1500, easing: "ease-in-out" });
 
-    // Cleanup function
     return () => {
       AOS.refresh();
     };
   }, []);
-  // https://cdn.pixabay.com/photo/2016/11/27/21/42/stock-1863880_1280.jpg
+
   const servicesCard = [
-    {
-      name: t("services.marketingPlan"),
-      description: t("services.marketingPlanDescription"),
-      img: service1,
-    },
-    {
-      name: t("services.digitalMarketing"),
-      description: t("services.digitalMarketingDescription"),
-      img: service2,
-    },
-    {
-      name: t("services.socialMedia"),
-      description: t("services.socialMediaDescription"),
-      img: service3,
-    },
-    {
-      name: t("services.creativeDesign"),
-      description: t("services.creativeDesignDescription"),
-      img: service4,
-    },
-    {
-      name: t("services.seo"),
-      description: t("services.seoDescription"),
-      img: service5,
-    },
-    {
-      name: t("services.contentWriting"),
-      description: t("services.contentWritingDescription"),
-      img: service6,
-    },
-    {
-      name: t("services.developeSites"),
-      description: t("services.developeSitesDescriptiion"),
-      img: service7,
-    },
-    {
-      name: t("services.emailMarketing"),
-      description: t("services.emailMarketingDescription"),
-      img: service8,
-    },
-    {
-      name: t("services.marketingConsulting"),
-      description: t("services.marketingConsultingDescription"),
-      img: service9,
-    },
-    {
-      name: t("services.dataAnalyisis"),
-      description: t("services.dataAnalyisisDescription"),
-      img: service10,
-    },
-    {
-      name: t("services.mobileApps"),
-      description: t("services.mobileAppsDescription"),
-      img: service11,
-    },
-    {
-      name: t("services.hosting"),
-      description: t("services.hostingDescription"),
-      img: service12,
-    },
+    { name: t("services.marketingPlan"), description: t("services.marketingPlanDescription"), img: service1 },
+    { name: t("services.digitalMarketing"), description: t("services.digitalMarketingDescription"), img: service2 },
+    { name: t("services.socialMedia"), description: t("services.socialMediaDescription"), img: service3 },
+    { name: t("services.creativeDesign"), description: t("services.creativeDesignDescription"), img: service4 },
+    { name: t("services.seo"), description: t("services.seoDescription"), img: service5 },
+    { name: t("services.contentWriting"), description: t("services.contentWritingDescription"), img: service6 },
+    { name: t("services.developeSites"), description: t("services.developeSitesDescriptiion"), img: service7 },
+    { name: t("services.emailMarketing"), description: t("services.emailMarketingDescription"), img: service8 },
+    { name: t("services.marketingConsulting"), description: t("services.marketingConsultingDescription"), img: service9 },
+    { name: t("services.dataAnalyisis"), description: t("services.dataAnalyisisDescription"), img: service10 },
+    { name: t("services.mobileApps"), description: t("services.mobileAppsDescription"), img: service11 },
+    { name: t("services.hosting"), description: t("services.hostingDescription"), img: service12 },
   ];
+
+  const [hoveredCard, setHoveredCard] = useState(null);
+
   return (
-    <div
-      className={`relative z-[1] pt-[140px]  bg-[#051118] p-2`}
-      id="services"
-    >
-      <div
-        className="absolute inset-0 bg-cover bg-center opacity-5"
-        style={{
-          backgroundImage: `url(${bgImg})`,
-        }}
-      ></div>
-      <h2
-        className="relative w-fit mx-auto  text-3xl font-semibold leading-10	drop-shadow-md mb-12 capitalize text-[#b7e4ea] text-center"
+    <div className="relative z-[1] pt-[140px] bg-[#051118] p-2" id="services">
+      <div className="absolute inset-0 bg-cover bg-center opacity-5" style={{ backgroundImage: `url(${bgImg})` }}></div>
+      <div className="mt-10 mb-20">
+
+      <SliderComponent />
+      </div>
+
+      {/* <h2
+        className={`relative mx-auto text-3xl font-semibold leading-10 drop-shadow-md mb-12 capitalize text-[#b7e4ea] text-center ${
+          direction === "rtl" ? "right-0" : "left-0"
+        } w-[50%]`}
         data-aos="fade-left"
       >
-        <svg
-          className={`absolute w-[200px] h-[200px] top-[-82px] left-[-35px]  opacity-50 z-[-1]	`}
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          // xmlns:xlink="http://www.w3.org/1999/xlink"
-          // xmlns:svgjs="http://svgjs.dev/svgjs"
-          viewBox="0 0 2000 2000"
-        >
-          <g>
-            <g fill="hsl(194, 45%, 50%)" id="star">
-              <path
-                d="M 500 500 C 1000 1000 1000 1000 1300 750 C 1000 1000 1000 1000 2000 2000 C 1000 1000 1000 1000 750 1300 C 1000 1000 1000 1000 500 500"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </g>
-          </g>
-        </svg>
         {t("services.title")}
       </h2>
-      <p
-        className="text-white text-center mb-12 max-w-[80%] md:max-w-[50%] mx-auto"
-        data-aos="fade-left"
-      >
+      <p className="text-white text-center mb-12 max-w-[80%] md:max-w-[50%] mx-auto" data-aos="fade-left">
         {t("services.description")}
-      </p>
+      </p> */}
 
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-10 mx-auto px-[15px] mb-16">
-        {servicesCard.map((card, i) => {
-          return (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto px-[15px] mb-16 md:max-w-[90%] lg:max-w-[70%] cursor-pointer">
+        {servicesCard.map((card, i) => (
+          <div
+            key={i}
+            className={`relative rounded-lg bg-cover bg-center group h-36 md:h-64 flex items-center justify-center overflow-hidden`}
+            style={{ backgroundImage: `url(${card.img})` }}
+            onMouseEnter={() => setHoveredCard(i)}
+            onMouseLeave={() => setHoveredCard(null)}
+          >
             <div
-              data-aos="fade-up"
-              key={i}
-              className={`group relative before:${
-                direction === "ltr" ? "bg-gradient-to-tr" : "bg-gradient-to-tl"
-              }
-              } before:from-[#051118] before:to-[#103743] before:content-[''] before:absolute before:h-full before:transition-[0.3s] before:rounded-lg before:w-[calc(100%_-_60px)] before:z-[-2] ${
-                direction === "ltr" ? "before:right-0" : "before:left-0"
-              } before:top-0 after:content-[''] after:absolute after:h-full after:transition-[0.3s] after:rounded-lg ${
-                direction === "ltr" ? "after:right-0" : "after:left-0"
-              } after:top-0 after:z-[-1] after:bg-[#11333d] after:w-0 hover:after:w-[calc(100%_-_60px)]`}
-            >
-              <div className="flex items-center pt-[60px]">
-                <img
-                  src={card.img}
-                  alt=""
-                  className="group-hover:grayscale	 w-[calc(100%_-_60px)] transition-[0.3s] rounded-[15px]"
-                />
-                <div className="flex flex-col items-center gap-5">
-                  <div className="w-[60px] h-[30px] flex justify-center items-center">
-                    <FaChartLine className="text-[#b7e4ea] transition-[0.3s] group-hover:z-10 icon" />
-                  </div>
-                  <div className="w-[60px] h-[30px] flex justify-center items-center">
-                    <FaLightbulb className="text-[#b7e4ea] transition-[0.3s] group-hover:z-10 icon" />
-                  </div>
-                  <div className="w-[60px] h-[30px] flex justify-center items-center">
-                    <FaTasks className="text-[#b7e4ea] transition-[0.3s] group-hover:z-10 icon" />
-                  </div>
-                  <div className="w-[60px] h-[30px] flex justify-center items-center">
-                    <FaRegClock className="text-[#b7e4ea] transition-[0.3s] group-hover:z-10 icon" />
-                  </div>
+              className={`absolute inset-0 bg-gradient-to-tr from-[#051118] to-[#103743] opacity-50 transition-opacity duration-300`}
+            ></div>
+            <Transition in={hoveredCard === i} timeout={300} mountOnEnter unmountOnExit>
+              {(state) => (
+                <div className={`absolute inset-0 flex items-center justify-center text-white transition-opacity duration-500 ${state === "entered" ? "opacity-100" : "opacity-0"}`}>
+                  <p className={`absolute left-4 ${
+                    direction === "ltr" ? "left-4" : "right-4"
+                  } w-[90%] md:w-[60%] text-sm md:text-lg`} 
+                  data-aos="fade-right"
+                  >{card.description}</p>
                 </div>
-              </div>
-              <div className={`${direction === "ltr" ? "pl-20" : "pr-20"}`}>
-                {/* Title */}
-                <h3 className="mt-3.5 font-black py-3 mx-2 text-[#b7e4ea] text-[18px] mb-0">
+              )}
+            </Transition>
+            <Transition in={hoveredCard !== i} timeout={300} mountOnEnter unmountOnExit>
+              {(state) => (
+                <h3
+                  className={`absolute transition-opacity duration-500 ${
+                    direction === "ltr" ? "left-4" : "right-4"
+                  } w-[50%] ${
+                    state === "entered" ? "opacity-100" : "opacity-0"
+                  } text-xl md:text-3xl text-white`}
+                  data-aos="fade-left"
+                >
                   {card.name}
                 </h3>
-                {/* Description */}
-                <p className="mb-3.5 text-[#fffffc] text-[15px] mx-2">
-                  {card.description}
-                </p>
-              </div>
-            </div>
-          );
-        })}
+              )}
+            </Transition>
+          </div>
+        ))}
       </div>
       <Footer />
     </div>
