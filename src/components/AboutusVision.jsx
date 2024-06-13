cc52fc7c0ce8148c7983678379d3691e83a83b03
Mohamed Z <mz7891555@gmail.com>
2024-06-14 01:56:24 +0300

    Update AboutUs

--- src/components/AboutusVision.jsx
+++ src/components/AboutusVision.jsx
@@ -10,24 +10,22 @@ export default function AboutusVision({ aboutBg, title, description }) {
     <div
       className="relative mt-32 lg:h-[400px] flex items-center justify-center mx-auto w-[90%] md:w-[80%] max-w-5xl rounded-lg shadow-lg overflow-hidden"
     >
-       <img
-              src={aboutBg}
-              // alt={card.name}
-              className="absolute z-[-1] inset-0 object-cover w-full h-full transition-transform duration-300 transform scale-x-100"
-              style={{ transform: direction === "rtl" ? "scaleX(1)" : "scaleX(-1)" }}
-            />
+      <img
+        src={aboutBg}
+        className="absolute z-[-1] inset-0 object-cover w-full h-full transition-transform duration-300 transform scale-x-100"
+        style={{ transform: direction === "rtl" ? "scaleX(1)" : "scaleX(-1)" }}
+      />
       <div className="absolute inset-0 bg-black opacity-40"></div>
-      <div className="absolute inset-0"></div>
       <div className="relative p-6 md:p-12 z-10 text-white w-full h-full flex items-center">
         <div
           className={`flex flex-col ${
             direction === "ltr" ? "mr-auto text-left" : "ml-auto text-right"
-          } md:w-1/2`}
+          } md:w-1/2 w-full`}
         >
-          <h2 className="text-2xl md:text-4xl font-bold mb-4">
+          <h2 className="text-xl sm:text-2xl md:text-4xl font-bold mb-4">
             {title}
           </h2>
-          <p className="mb-4 text-base md:text-lg">
+          <p className="text-sm sm:text-base md:text-lg mb-4">
             {description}
           </p>
         </div>
